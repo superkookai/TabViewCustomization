abc2a90542c178994a6313e5f8b432baabf7da68
Weerawut <weerawut@live.com>
2024-12-10 12:34:09 +0700

    Initial Commit

---
+++ TabViewCustomization/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabViewCustomization
+//
+//  Created by Weerawut Chaiyasomboon on 10/12/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabViewCustomization/TabViewCustomizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewCustomizationApp.swift
+//  TabViewCustomization
+//
+//  Created by Weerawut Chaiyasomboon on 10/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct TabViewCustomizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
